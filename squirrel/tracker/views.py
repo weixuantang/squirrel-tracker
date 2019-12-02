48c03a53ade52debbb21de7a1e6eab8b61ba3fcc
Stella Zhang <jz3156@columbia.edu>
2019-12-02 20:30:31 +0000

    Complete the map showing the squirrel locations

--- squirrel/tracker/views.py
+++ squirrel/tracker/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+import random
+from .models import Squirrel
 # Create your views here.
+def showmap(request):
+    templist = random.sample(list(Squirrel.objects.all()),100)
+    sightings = []
+    for temp in templist:
+        sightings.append({'latitude':temp.y, 'longitude': temp.x})
+    context = {'sightings':sightings}
+    return render(request,'tracker/map.html',context)
