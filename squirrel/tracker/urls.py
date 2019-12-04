db40c0728bc4201ac21ea501e6a902b7e54fb165
Stella Zhang <jz3156@columbia.edu>
2019-12-04 20:54:42 +0000

    Complete the stats page.

--- squirrel/tracker/urls.py
+++ squirrel/tracker/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('map/',views.showmap, name='showmap')
+        path('map/',views.showmap, name='showmap'),
+        path('sightings/add',views.add, name='add'),
+        path('sightings/added',views.added, name='added'),#temporary setting
+        path('sightings/stats',views.showstats, name='showstats'),
         ]
